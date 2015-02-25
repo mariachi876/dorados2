771d837a5321e1bcb99f3edf3487f3104331f562
soujiro27 <jc.raus27@gmail.com>
2015-02-25 14:56:38 -0600

    menu terminado

--- js/funciones.js
+++ js/funciones.js
@@ -3,9 +3,34 @@ $(document).on('ready',inicio);
 function inicio()
 {
 	$('#licenciatura').on('click',licenciatura);
+	$('#abierto').on('click',abierto);
+	$('#maestrias').on('click',maestrias);
+	$('#doctorado').on('click',doctorado);
+	$('#menu').on('click',menu);
 }
 
 function licenciatura()
 {
 	$('ul.subMenuLicenciatura').toggle('slow');
+}
+
+function abierto()
+{
+	
+	$('ul.subMenuAbierto').toggle('slow');
+}
+
+function maestrias()
+{
+	$('ul.subMenuMaestrias').toggle('slow');
+}
+function doctorado()
+{
+	$('ul.subMenuDoctorado').toggle('slow');
+	
+}
+
+function menu()
+{
+	$('header nav').toggle('slow');
 }
